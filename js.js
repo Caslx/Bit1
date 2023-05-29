f913ddde774077a67c0e34ad5a1ea399995af640
Camilo Caslx <camilonejy@gmail.com>
2023-05-29 17:04:21 -0500

    Commit message

--- js.js
+++ js.js
@@ -1,24 +1,5 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const shineOverlays = document.querySelectorAll('.shine-overlay');
-    const toggleButton = document.getElementById('toggleShine');
+$(".option").click(function(){
+  $(".option").removeClass("active");
+  $(this).addClass("active");
   
-    // Hover Effect
-    shineOverlays.forEach((overlay) => {
-      overlay.addEventListener('mouseenter', () => {
-        const fruitColor = overlay.parentNode.querySelector('img').getAttribute('alt');
-        overlay.style.background = fruitColor;
-      });
-  
-      overlay.addEventListener('mouseleave', () => {
-        overlay.style.background = 'transparent';
-      });
-    });
-  
-    // Button Functionality
-    toggleButton.addEventListener('click', () => {
-      shineOverlays.forEach((overlay) => {
-        overlay.classList.toggle('shining');
-      });
-    });
-  });
-  
+});
